52195ffa72d5e879821d8337a9988638084919a4
Enigma <can998@yandex.ru>
2023-07-25 17:16:59 +0400

    1
    `

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя!");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию!");
+            string Surname = Console.ReadLine();
+            Console.WriteLine("Введите возраст!");
+            string Age = Console.ReadLine();
+            Console.WriteLine("Введите рост!");
+            string Height = Console.ReadLine();
+            Console.WriteLine("Введите вес!");
+            string Weight = Console.ReadLine();
+            string Result_a = "Ответ А Конкатенация строк  " + Name + " " + Surname + " " + Age + " " + Height + " " + Weight;
+            Console.WriteLine(Result_a);
+            Console.WriteLine(" форматированный вывод {0} {1} {2} {3} {4}", Name, Surname, Age, Height, Weight);
+            Console.WriteLine($"Ответ В Инетрполяция строк {Name} {Surname} {Age} {Height} {Weight}");
+            Console.ReadLine();
+        }
+    }
+}
