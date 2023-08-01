ba14429a130b2f728938d417938395c0b3d9e794
Enigma <can998@yandex.ru>
2023-08-02 01:10:10 +0400

    DZ_3

---
+++ DZ_SHARP_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_SHARP_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
